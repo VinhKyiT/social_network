c138f8aac8e4742ab98a29372b2e6a6d4da03370
VinhKyIT <vinhkyit2905@gmail.com>
2022-07-03 17:21:45 +0700

    done post ui

--- src/components/post/post.style.ts
+++ src/components/post/post.style.ts
@@ -54,5 +54,47 @@ const styles = StyleSheet.create({
     height: verticalScale(200),
     borderRadius: scale(12),
   },
+  statisticCountWrapper: {
+    flexDirection: 'row',
+    marginTop: verticalScale(10),
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  reactionCount: {
+    flexDirection: 'row',
+  },
+  textReactionCount: {
+    fontSize: scale(14),
+    fontFamily: AppTheme.Fonts.Primary.Regular,
+    color: AppTheme.Colors.TextBlack,
+    marginLeft: scale(5),
+  },
+  commentAndShare: {
+    flexDirection: 'row',
+  },
+  textCmtShare: {
+    fontSize: scale(14),
+    fontFamily: AppTheme.Fonts.Primary.Regular,
+    color: AppTheme.Colors.TextSecondary,
+  },
+  postActionArea: {
+    flexDirection: 'row',
+    marginTop: verticalScale(10),
+    paddingTop: verticalScale(10),
+    borderTopColor: AppTheme.Colors.Border,
+    borderTopWidth: 1,
+  },
+  postActionItem: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  textPostAction: {
+    fontSize: scale(14),
+    fontFamily: AppTheme.Fonts.Primary.Regular,
+    color: AppTheme.Colors.TextBlack,
+    marginLeft: scale(5),
+  },
 });
 export default styles;
--- src/components/post/post.view.tsx
+++ src/components/post/post.view.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import styles from './post.style';
 import FastImage from 'react-native-fast-image';
 import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { scale } from '~utils/Scale';
 import { AppTheme } from '~configs/AppTheme';
 const PostView = (item: any) => {
@@ -39,6 +40,47 @@ const PostView = (item: any) => {
             style={styles.postMedia}
             resizeMode={FastImage.resizeMode.cover}
           />
+        </View>
+        <View style={styles.statisticCountWrapper}>
+          <View style={styles.reactionCount}>
+            <FontAwesome
+              name="heart-o"
+              size={scale(20)}
+              color={AppTheme.Colors.TextBlack}
+            />
+            <Text style={styles.textReactionCount}>50</Text>
+          </View>
+          <View style={styles.commentAndShare}>
+            <Text style={styles.textCmtShare}>10 comments</Text>
+            <Text style={styles.textCmtShare}> • </Text>
+            <Text style={styles.textCmtShare}>20 shares</Text>
+          </View>
+        </View>
+        <View style={styles.postActionArea}>
+          <TouchableOpacity style={styles.postActionItem}>
+            <FontAwesome
+              name="thumbs-o-up"
+              size={scale(20)}
+              color={AppTheme.Colors.TextBlack}
+            />
+            <Text style={styles.textPostAction}>Like</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.postActionItem}>
+            <FontAwesome
+              name="comment-o"
+              size={scale(20)}
+              color={AppTheme.Colors.TextBlack}
+            />
+            <Text style={styles.textPostAction}>Comment</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.postActionItem}>
+            <FontAwesome
+              name="share-square-o"
+              size={scale(20)}
+              color={AppTheme.Colors.TextBlack}
+            />
+            <Text style={styles.textPostAction}>Share</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
